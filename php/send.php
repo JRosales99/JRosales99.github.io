96ed4a5be400db416f615b7352835f968c043c88
JRosales99 <126920709+JRosales99@users.noreply.github.com>
2023-03-18 22:19:27 -0600

    Update send.php

--- php/send.php
+++ php/send.php
@@ -1,23 +1,60 @@
 <?php
 
-// Llamando a los campos
-$nombre = $_POST['nombre'];
-$telefono = $_POST['telefono'];
-$correo = $_POST['correo'];
-$mensaje = $_POST['mensaje'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Obtener los campos del formulario y filtrar los datos
+  $nombre = htmlspecialchars(trim($_POST["nombre"]));
+  $telefono = htmlspecialchars(trim($_POST["telefono"]));
+  $correo = htmlspecialchars(trim($_POST["correo"]));
+  $mensaje = htmlspecialchars(trim($_POST["mensaje"]));
 
-// datos para el correo
-$destinatario = "jaffryjosuerosales@gmail.com";
-$asunto = "Contacto desde la página web";
+  // Validar los datos del formulario
+  $errores = [];
 
-$carta = "De: $nombre \n";
-$carta .= "Correo: $correo \n";
-$carta .= "Telefono: $telefono \n"; // Agregar un salto de línea aquí
-$carta .= "Mensaje: $mensaje";
+  if (empty($nombre)) {
+    $errores[] = "El nombre es obligatorio.";
+  }
 
-// Enviando mensaje
-if (mail($destinatario, $asunto, $carta)) {
-  header('Location: mensaje-enviado.html');
+  if (empty($telefono)) {
+    $errores[] = "El teléfono es obligatorio.";
+  }
+
+  if (empty($correo) || !filter_var($correo, FILTER_VALIDATE_EMAIL)) {
+    $errores[] = "El correo electrónico no es válido.";
+  }
+
+  if (empty($mensaje)) {
+    $errores[] = "El mensaje es obligatorio.";
+  }
+
+  // Si hay errores, mostrarlos en la página de contacto
+  if (!empty($errores)) {
+    foreach ($errores as $error) {
+      echo "<p>$error</p>";
+    }
+  } else {
+    // Enviar el correo electrónico si no hay errores
+    $destinatario = "jaffryjosuerosales@gmail.com";
+    $asunto = "Contacto desde la página web";
+
+    $cabeceras = "From: $nombre <$correo>\r\n";
+    $cabeceras .= "Reply-To: $correo\r\n";
+
+    $mensaje_correo = "Nombre: $nombre\n";
+    $mensaje_correo .= "Teléfono: $telefono\n";
+    $mensaje_correo .= "Correo: $correo\n\n";
+    $mensaje_correo .= "Mensaje:\n$mensaje";
+
+    // Enviar el correo electrónico
+    if (mail($destinatario, $asunto, $mensaje_correo, $cabeceras)) {
+      header("Location: mensaje-enviado.html");
+    } else {
+      echo "<p>Error al enviar el mensaje. Por favor, inténtelo de nuevo más tarde.</p>";
+    }
+  }
 } else {
-  echo "Error al enviar el mensaje";
+  // Si se intenta acceder al archivo PHP directamente, redirigir a la página de contacto
+  header("Location: contacto.html");
 }
+
+?>
+
