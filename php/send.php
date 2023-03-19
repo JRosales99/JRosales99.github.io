1546fe60a31f7e89b7b7430659f753f96b7e9abc
JRosales99 <126920709+JRosales99@users.noreply.github.com>
2023-03-18 22:13:48 -0600

    Update send.php

--- php/send.php
+++ php/send.php
@@ -1,6 +1,6 @@
 <?php
 
-//Llamando a los campos
+// Llamando a los campos
 $nombre = $_POST['nombre'];
 $telefono = $_POST['telefono'];
 $correo = $_POST['correo'];
@@ -8,14 +8,16 @@ $mensaje = $_POST['mensaje'];
 
 // datos para el correo
 $destinatario = "jaffryjosuerosales@gmail.com";
-$asunto = "Contacto desde la pagina web";
+$asunto = "Contacto desde la página web";
 
 $carta = "De: $nombre \n";
 $carta .= "Correo: $correo \n";
-$carta .= "Telefono: $telefono";
+$carta .= "Telefono: $telefono \n"; // Agregar un salto de línea aquí
 $carta .= "Mensaje: $mensaje";
 
-//Enviando mensaje
-mail($destinatario, $asunto, $carta);
-header('Location:mensaje-enviado.html')
-?>
+// Enviando mensaje
+if (mail($destinatario, $asunto, $carta)) {
+  header('Location: mensaje-enviado.html');
+} else {
+  echo "Error al enviar el mensaje";
+}
