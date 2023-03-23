3b71c35afe259878657641c620f71f9c75849719
JRosales99 <126920709+JRosales99@users.noreply.github.com>
2023-03-22 18:28:20 -0600

    act

--- php/send.php
+++ php/send.php
@@ -1,23 +1,12 @@
-<?php
+<?php 
+	$nombre = $_POST['nombre'];
+	$telefono = $_POST['telefono'];
+  $correo = $_POST['email'];
+	$asunto = 'Formulario Rellenado';
+	$mensaje = "Nombre: ".$nombre."<br> Email: $correo <br> Mensaje:".$_POST['mensaje'];
 
-// Llamando a los campos
-$nombre = $_POST['nombre'];
-$telefono = $_POST['telefono'];
-$correo = $_POST['correo'];
-$mensaje = $_POST['mensaje'];
 
-// datos para el correo
-$destinatario = "jaffryjosuerosales@gmail.com";
-$asunto = "Contacto desde la página web";
-
-$carta = "De: $nombre \n";
-$carta .= "Correo: $correo \n";
-$carta .= "Telefono: $telefono \n"; // Agregar un salto de línea aquí
-$carta .= "Mensaje: $mensaje";
-
-// Enviando mensaje
-if (mail($destinatario, $asunto, $carta)) {
-  header('Location: mensaje-enviado.html');
-} else {
-  echo "Error al enviar el mensaje";
-}
+	if(mail('jaffryjosuerosales@gmail.com', $asunto, $mensaje)){
+		echo "Correo enviado";
+	}
+ ?>
