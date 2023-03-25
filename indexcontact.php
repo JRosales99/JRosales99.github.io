c173f246316970a90689349b1f0875347f4eec32
JRosales99 <126920709+JRosales99@users.noreply.github.com>
2023-03-24 19:14:14 -0600

    Actualizacion

--- indexcontact.php
+++ indexcontact.php
@@ -11,6 +11,8 @@
     <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.17/dist/sweetalert2.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.17/dist/sweetalert2.all.min.js"></script>
     <script src="./Javascript/main.js"></script>
     <script src="js/jquery-3.2.1.js"></script>
     <script src="js/script.js"></script>
@@ -52,28 +54,39 @@
                 <h2>INFORMACION<br>DE CONTACTO</h2>
             </section>
             <section class="info_items">
-                <p><span class="fa fa-envelope"></span> info.contact@gmail.com</p>
-                <p><span class="fa fa-mobile"></span> +1(585) 902-8665</p>
+                <p><span class="fa fa-envelope"></span>GRUPO DEL VALLE</p>
+                <p><span class="fa fa-mobile"></span>+504 ****-****</p>
             </section>
         </section>
 
         <form method="POST" class="form_contact">
             <h2>Envia un mensaje</h2>
             <div class="user_info">
-                <label for="names">Nombres *</label>
-                <input type="text" id="names" name="nombre" required>
+              <label for="names">Nombres</label>
+              <input type="text" id="names" name="nombre" required>
+      
+              <label for="phone">Telefono</label>
+              <input type="text" id="phone" name="telefono" required>
+      
+              <label for="email">Correo electronico</label>
+              <input type="text" id="email" name="email">
 
-                <label for="phone">Telefono / Celular</label>
-                <input type="text" id="phone" name="telefono" required>
+              <label for="empresa">Empresa</label>
+              <select id="empresa" name="empresa">
+                <option value="" selected disabled>Seleccione una opción</option>
+                <option value="Bienes Raices Olanchito">Bienes Raices Olanchito</option>
+                <option value="Deprovah">Deprohva</option>
+                <option value="Jardin de paz san jorge">Jardin De Paz San Jorge</option>
+                <option value="Texaco las americas">Texaco Las Americas</option>
+                <option value="Caja Rural de ahorro y credito">Caja Rural de Ahorro y Credito</option>
+                <option value="Texaco S3-Picnic">Texaco S3-Picnic</option>
+              </select>
 
-                <label for="email">Correo electronico *</label>
-                <input type="text" id="email" name="email">
-
-                <label for="mensaje">Mensaje *</label>
-                <textarea id="mensaje" name="mensaje" required></textarea>
-
-                <input type="submit" value="Enviar Mensaje" id="btnSend" name="enviar_mensaje">
-            </div>
+              <label for="mensaje">Mensaje</label>
+              <textarea id="mensaje" name="mensaje" required></textarea>
+             
+              <input type="submit" value="Enviar Mensaje" id="btnSend" name="enviar_mensaje">
+          </div>
         </form>
         <?php
           include("registrar.php")
--- registrar.php
+++ registrar.php
@@ -4,25 +4,48 @@ include("con_db.php");
 if (isset($_POST['enviar_mensaje'])) {
     if (strlen($_POST['nombre']) >= 1 &&
         strlen($_POST['telefono']) >= 1 &&
-        strlen($_POST['email']) >= 1 &&
+        strlen($_POST['empresa']) >= 1 &&
         strlen($_POST['mensaje']) >= 1) {
 
         $nombre = trim($_POST['nombre']);
         $telefono = trim($_POST['telefono']);
         $email = trim($_POST['email']);
+        $empresa = trim($_POST['empresa']);
         $mensaje = trim($_POST['mensaje']);
         $fecha_reg = date("d/m/y");
-        $consulta = "INSERT INTO `datos obtenidos`(`nombre`, `telefono`, `email`, `mensaje`, `fecha_reg`)  VALUES ('$nombre','$telefono','$email','$mensaje','$fecha_reg')";
+        $consulta = "INSERT INTO `datos obtenidos`(`nombre`, `telefono`, `email`, `empresa`, `mensaje`, `fecha_reg`)  VALUES ('$nombre','$telefono','$email','$empresa','$mensaje','$fecha_reg')";
         $resultado = mysqli_query($conex, $consulta);
+
         if ($resultado) {
             ?>
-            <h3 class="enviado">Tu mensaje se envió correctamente, pronto un asesor se pondrá en contacto contigo.</h3>
+            <script>
+                $(document).ready(function(){
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Tu mensaje se envió correctamente',
+                        text: 'Pronto un asesor se pondrá en contacto contigo.',
+                        timer: 5000,
+                        showConfirmButton: false
+                    });
+                });
+            </script>
             <?php
         } else {
             ?>
-            <h3 class="bad">¡Ups, ha ocurrido un error, verifica tus datos y rellena todo el formulario!</h3>
+            <script>
+                $(document).ready(function(){
+                    Swal.fire({
+                        icon: 'error',
+                        title: '¡Ups, ha ocurrido un error!',
+                        text: 'Verifica tus datos y rellena todo el formulario.',
+                        timer: 5000,
+                        showConfirmButton: false
+                    });
+                });
+            </script>
             <?php
         }
     }
 }
 ?>
+
